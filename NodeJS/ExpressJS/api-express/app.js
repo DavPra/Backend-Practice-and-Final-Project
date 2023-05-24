6908bef0b1085a6228eaad668ccd37efe344ba65
David Praschak <david.praschak@univie.ac.at>
2023-05-24 21:05:32 +0200

    body parser implemented

--- NodeJS/ExpressJS/api-express/app.js
+++ NodeJS/ExpressJS/api-express/app.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const app = express();
 const db = require('mysql');
+const bodyParser = require('body-parser');
 
-db.createConnection({
+const con= db.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
     password: '',
     database: 'test_db'
+});
+
+con.connect((err) => {
+    if(err) console.log('Connection to database failed.');
+});
+
+app.get('/api/user', (req, res) => {
+    con.query('SELECT * FROM user', (err, rows) => {
+        res.send(JSON.stringify(result))
+    });  
+});
+
+app.get('/api/user/:id', (req, res) => {
+    con.query(`SELECT * FROM user WHERE id =${req.params.id}`, (err, result) => {
+        res.send(JSON.stringify(result[0]))
+    });
+
+});
+
+app.post('/api/user', (req, res) => {
+
+});
+
+app.delete('/api/user/:id', (req, res) => {
+
+});
+
+app.put('/api/user/:id', (req, res) => {
+
 });
 
 app.listen(3000, () => {
