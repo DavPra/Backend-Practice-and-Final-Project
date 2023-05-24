6be029a34f1a578ace7be33f8ebfd495016e300e
David Praschak <david.praschak@univie.ac.at>
2023-05-24 20:36:32 +0200

    package json angelegt, sql  installiert

--- NodeJS/ExpressJS/api-express/app.js
+++ NodeJS/ExpressJS/api-express/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const db = require('mysql');
+
+db.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'test_db'
+});
 
 app.listen(3000, () => {
     console.log('Server run on http://localhost:3000');
 })
+
+
