60c0db006475d0227365a2c913459fe19bbd04a1
David Praschak <david.praschak@univie.ac.at>
2023-05-30 19:48:55 +0200

    routes implemented

---
+++ NodeJS/ejs/Router/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const db = require('mysql');
+
+const con= db.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'testdb'
+});
+
+con.connect();
+
+router.get('/', (req, res) => {
+    con.query(`SELECT * FROM user`, (err, result) => {
+        res.send(JSON.stringify(result))
+    });  
+});
