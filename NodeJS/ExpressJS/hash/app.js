782b8b7468f34da361699e9f6f5339462e7d5ddd
David Praschak <david.praschak@univie.ac.at>
2023-05-31 20:24:11 +0200

    passport implemented

--- NodeJS/ExpressJS/hash/app.js
+++ NodeJS/ExpressJS/hash/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const register = require('./router/register');
 const login = require('./router/login');
+const passport = require('passport');
+
 
 app.use(express.json());
+app.use(passport.initialize());
 
 app.use('/api', register);
 app.use('/api', login);
--- NodeJS/ExpressJS/hash/router/login.js
+++ NodeJS/ExpressJS/hash/router/login.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const database = require('../services/database');
+const jwt = require('../services/jwt');
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
     try {
         const user = await database.findUserByCredetials(req.body);
-        res.json(user);
+        const token = jwt.signUser(user);
+        res.json({Token: token, user: user});
     } catch (e) {
         console.log(e);
         res.status(500).send('Something went wrong!');
