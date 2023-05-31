08a38ffef1ce6500f75e04d56390d2af2a42f582
David Praschak <david.praschak@univie.ac.at>
2023-05-31 21:03:49 +0200

    jwt strategy implemented

--- NodeJS/ExpressJS/hash/app.js
+++ NodeJS/ExpressJS/hash/app.js
@@ -4,12 +4,14 @@ const register = require('./router/register');
 const login = require('./router/login');
 const passport = require('passport');
 const localStrategy = require('./strategies/localstrategy');
+const jwtStrategy = require('./strategies/jwtStrategy');
 
 
 app.use(express.json());
 app.use(passport.initialize());
 
-passport.use(localStrategy);
+passport.use('local', localStrategy);
+passport.use('jwt', jwtStrategy);
 
 app.use('/api', register);
 app.use('/api', login);
