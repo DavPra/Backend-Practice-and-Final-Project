65eb18f5c51f30695e8dc97e49ab356b9f9a87cd
David Praschak <david.praschak@univie.ac.at>
2023-07-03 19:49:03 +0200

    login fixed

--- BackendAbschluss/router/login.js
+++ BackendAbschluss/router/login.js
@@ -7,7 +7,6 @@ router.post('/login', passport.authenticate('local', {session:false}), async (re
     try {
         const token = jwt.signUser(req.user)
         res.json({token:token, user:req.user})
-        res.status(200).send('Login successful')
     } catch (e) {
         console.log(e)
         res.status(500).send('Something went wrong')
