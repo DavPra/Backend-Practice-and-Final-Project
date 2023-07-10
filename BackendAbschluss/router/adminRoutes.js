ae565d31be6ede0a28bed36a8e0b2ab2b8cfac74
David Praschak <david.praschak@univie.ac.at>
2023-07-10 20:20:32 +0200

    adminroutes fixed

--- BackendAbschluss/router/adminRoutes.js
+++ BackendAbschluss/router/adminRoutes.js
@@ -24,4 +24,119 @@ router.get('/orders', passport.authenticate('jwt', { session: false }),
     }
 );
 
+router.post('/orders', passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+        try {
+            const adminStatus = req.user.admin;
+            if (adminStatus === true) {
+
+                    const orders = await db.addOrders(req.body);
+                    res.json(orders);
+
+            } else {
+                res.json({ message: 'You are not an admin' });
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).send('Something went wrong');
+        }
+    }
+);
+
+router.delete('/orders/:id', passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+        try {
+            const adminStatus = req.user.admin;
+            if (adminStatus === true) {
+
+                    const orders = await db.deleteOrders(req.params.id);
+                    res.json(orders);
+
+            } else {
+                res.json({ message: 'You are not an admin' });
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).send('Something went wrong');
+        }
+    }
+);
+
+router.patch('/orders/:id', passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+        try {
+            const adminStatus = req.user.admin;
+            if (adminStatus === true) {
+
+                    const orders = await db.updateOrders(req.params.id, req.body);
+                    res.json(orders);
+
+            } else {
+                res.json({ message: 'You are not an admin' });
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).send('Something went wrong');
+        }
+    }
+);
+
+router.patch('/products', passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+        try {
+            const adminStatus = req.user.admin;
+            if (adminStatus === true) {
+                
+                    const products = await db.updateProducts(req.body);
+                    res.json(products);
+
+            } else {
+                res.json({ message: 'You are not an admin' });
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).send('Something went wrong');
+        }
+    }
+);
+
+router.post('/products', passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+        try {
+            const adminStatus = req.user.admin;
+            if (adminStatus === true) {
+
+                    const products = await db.addProducts(req.body);
+                    res.json(products);
+
+            } else {
+                res.json({ message: 'You are not an admin' });
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).send('Something went wrong');
+        }
+    }
+);
+
+router.delete('/products/:id', passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+        try {
+            const adminStatus = req.user.admin;
+            if (adminStatus === true) {
+            
+                    const products = await db.deleteProducts(req.params.id);
+                    res.json(products);
+
+            } else {
+                res.json({ message: 'You are not an admin' });
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).send('Something went wrong');
+        }
+    }
+);
+
+
 module.exports = router;
--- BackendAbschluss/seeders/ordersSeed.js
+++ BackendAbschluss/seeders/ordersSeed.js
@@ -1,19 +1,16 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('orders', [{
-            OrderID: 1,
             OrderDate: '2020-01-01',
             OrderStatus: 'Offen',
             UserId: 2,
         },
         {
-            OrderID: 2,
             OrderDate: '2020-01-02',
             OrderStatus: 'Erledigt',
             UserId: 4,
         },
         {
-            OrderID: 3,
             OrderDate: '2020-01-03',
             OrderStatus: 'Offen',
             UserId: 3,
